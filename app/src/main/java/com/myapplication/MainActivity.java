0e9fb771e07256ad46131338e41d2e1b6f5df8bf
CharnyyAleksandrASU19bzu <65458983+CharnyyAleksandrASU19bzu@users.noreply.github.com>
2022-05-22 00:10:29 +0500

    Change

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -2,8 +2,14 @@ package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView imageView = findViewById(R.id.splash);
+        loadGif(imageView);
+    }
+    private void loadGif(ImageView iv) {
+        try {
+            ImageDecoder.Source source =
+                    ImageDecoder.createSource(getResources(), R.drawable.ic_splash);
+
+            Drawable drawable = ImageDecoder.decodeDrawable(source);
+            iv.setImageDrawable(drawable);
+
+            if (drawable instanceof AnimatedImageDrawable) {
+                ((AnimatedImageDrawable) drawable).start();
+                Toast.makeText(getApplicationContext(),
+                        "Animation started",
+                        Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(getApplicationContext(),
+                    "Animation not working!!!",
+                    Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "IOException: \n" + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
